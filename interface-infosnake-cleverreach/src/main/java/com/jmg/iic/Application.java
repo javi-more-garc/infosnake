69ed59d38f69a190cb15ba14f7322c6652855bfc
Javier Moreno Garcia <javi.more.garc@gmail.com>
2015-05-19 18:23:40 +0200

    CHANGE - Ignore properties outside jar if not found (i.e. don't raise exception)

--- interface-infosnake-cleverreach/src/main/java/com/jmg/iic/Application.java
+++ interface-infosnake-cleverreach/src/main/java/com/jmg/iic/Application.java
@@ -15,7 +15,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @PropertySources({ @PropertySource("classpath:/conf.properties"),
-		@PropertySource(value = "file:conf.properties") })
+		@PropertySource(value = "file:conf.properties", ignoreResourceNotFound = true) })
 public class Application {
 
 	public static void main(String[] args) {
